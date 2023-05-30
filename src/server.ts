a6ac02ad0b88e78e48fa10321a7b4c6ebfea370a
simple github <foysalahmed5965@gmail.com>
2023-05-30 14:35:39 +0600

    cors, mongoose and others

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import app from "./app";
+
+const mongoose = require('mongoose');
+import config from "./config/index";
+
+
+
+async function bootStrap() {
+    try {
+        await mongoose.connect(config.databaseUrl as string);
+        console.log('database connect succesfully')
+        app.listen(config.port, () => {
+            console.log(`Example app listening on port ${config.port}`)
+        })
+    }
+    catch (err) {
+        console.log('failed to connect database',(err))
+    }
+
+}
+bootStrap()
