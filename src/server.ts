14d1fe504628267b7d5e7e1f89863edd27c27e9e
simple github <foysalahmed5965@gmail.com>
2023-05-31 05:37:04 +0600

    testing

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,17 @@
-import app from "./app";
+import app from './app'
 
-const mongoose = require('mongoose');
-import config from "./config/index";
-
-
+const mongoose = require('mongoose')
+import config from './config/index'
 
 async function bootStrap() {
-    try {
-        await mongoose.connect(config.databaseUrl as string);
-        console.log('database connect succesfully')
-        app.listen(config.port, () => {
-            console.log(`Example app listening on port ${config.port}`)
-        })
-    }
-    catch (err) {
-        console.log('failed to connect database',(err))
-    }
-
+  try {
+    await mongoose.connect(config.databaseUrl as string)
+    console.log('database connect succesfully')
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`)
+    })
+  } catch (err) {
+    console.log('failed to connect database', err)
+  }
 }
 bootStrap()
